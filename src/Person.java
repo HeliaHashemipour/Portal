45337538a2b97e5e532beaccaa7fa2c4195c3e56
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 13:13:57 +0330

    complete Person class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,41 @@
+import java.util.Objects;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String id;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
