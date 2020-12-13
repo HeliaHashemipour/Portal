311916c42ecedcdf13b58baf4d79f6d4fac1ee9b
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 15:45:58 +0330

    complete setMainPanel method of AdminPanel class

--- src/AdminPanel.java
+++ src/AdminPanel.java
@@ -140,7 +140,14 @@ public class AdminPanel {
     }
 
     private void setMainPanel() {
+        mainPanel = new JPanel();
+        mainPanel.setBounds(250, 125, 750, 575);
+        mainPanel.setLayout(null);
 
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+        mainPanel.setBorder(border);
+
+        frame.add(mainPanel);
     }
 
     private void setPersonalPanel() {
