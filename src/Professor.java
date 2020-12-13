dd299288da768e224874b330f26a24e5ae8651ca
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 13:02:25 +0330

    add essential methods of professor class

--- src/Professor.java
+++ src/Professor.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Professor extends Person {
+    private final List<Classroom> classrooms;
+
+    public Professor() {
+
+    }
+
+    public boolean createClassroom(Classroom classroom) {
+
+    }
+
+    public void removeClassroom(Classroom classroom) {
+
+    }
+
+    public List<Student> getStudents() {
+
+    }
+
+    public void setGrade(Student student, Classroom classroom, int grade) {
+
+    }
+
+    public List<Classroom> getClassrooms() {
+
+    }
+
+
 }
