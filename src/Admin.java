2be6789cb662c851ab7d43f0ca4bb5142a43ee22
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 12:59:47 +0330

    add basic methods to the admin class

--- src/Admin.java
+++ src/Admin.java
@@ -1,2 +1,24 @@
+import java.util.List;
+
 public class Admin extends Person {
+    public void setMealPlan(Meal[] meals) {
+
+    }
+
+    public List<Student> getStudents() {
+
+    }
+
+    public List<Professor> getProfessors() {
+
+    }
+
+    public boolean addProfessor(Professor professor) {
+
+    }
+
+    public boolean addStudent(Student student) {
+        
+    }
+
 }
