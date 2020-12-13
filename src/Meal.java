0fabe6f35cbb0aab186f75023feef84b47065eb5
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 13:09:34 +0330

    add essential method to the Meal class

--- src/Meal.java
+++ src/Meal.java
@@ -1,2 +1,37 @@
+
+import java.util.List;
+
 public class Meal {
+
+    private String name;
+    private int cost;
+    private List<Student> students;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public boolean addStudent(Student st) {
+        if (students.contains(st))
+            return false;
+        return students.add(st);
+    }
 }
+
+
