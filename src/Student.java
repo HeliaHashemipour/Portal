cb2035a13364211ff1201d43c38f97fe429652d5
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 16:12:42 +0330

    complete Student class

--- src/Student.java
+++ src/Student.java
@@ -13,39 +13,61 @@ public class Student extends Person {
     }
 
     public void deposit(int amount) {
-
+       
     }
 
     public Meal[] getmeals() {
-
+        return MealPlan.getMeals();
     }
 
     public boolean reserveMeal(Meal meal) {
-
+        if (meal.addStudent(this) && MealPlan.indexOf(meal) != -1) {
+            this.meals[MealPlan.indexOf(meal)] = meal;
+            return true;
+        }
+        else
+            return false;
     }
 
     public boolean addUnit(Classroom classroom) {
-
-
-
+        int numberOfUnits = 0;
+        int averageGrade = 0, totalGrade = 0;
+        for (int i = 0; i < this.getUnits().size(); i++){
+            int currentUnitNumber = this.getUnits().get(i).getClassroom().getNumberOfUnit();
+            numberOfUnits += currentUnitNumber;
+            totalGrade += this.getUnits().get(i).getGrade()*currentUnitNumber;
+        }
+        averageGrade = totalGrade/numberOfUnits;
+        if (averageGrade < 17){
+            if (numberOfUnits + classroom.getNumberOfUnit() <= 20)
+                return this.units.add(classroom.getUnit());
+        }
+        if (averageGrade >= 17){
+            if (numberOfUnits + classroom.getNumberOfUnit() <= 24)
+                return this.units.add(classroom.getUnit());
+        }
+        return false;
     }
 
     public void setBalance(int balance) {
-
+        this.balance = balance;
     }
 
     public int getBalance() {
-
+        return balance;
     }
 
     public List<Unit> getUnits() {
-
+        return units;
     }
 
     public double averageGrade() {
-
-
-}
-
-
+        double sum = 0;
+        double unitSum = 0;
+        for (Unit unit : units) {
+            sum += unit.getGrade();
+            unitSum += unit.getClassroom().getNumberOfUnit();
+        }
+        return sum / unitSum;
+    }
 }
