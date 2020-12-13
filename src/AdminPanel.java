b52fa0c9468882ead32e90c58f4e4737c73e3fce
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 13:28:15 +0330

    add important methods to the AdminPanel class

--- src/AdminPanel.java
+++ src/AdminPanel.java
@@ -1,2 +1,80 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.*;
+import java.util.List;
+
 public class AdminPanel {
+    private JFrame frame;
+    private JPanel header;
+    private JPanel menu;
+    private JPanel mainPanel;
+    private JPanel personalPanel;
+    private JPanel mealPanel;
+    private JPanel studentsPanel;
+    private JPanel professorsPanel;
+    private JPanel classRoomsPanel;
+    private final Admin admin;
+
+    public AdminPanel(Admin admin) {
+
+    }
+
+    private void setHeader() {
+
+    }
+
+    private void setMenu() {
+
+    }
+
+    private void setMainPanel() {
+
+    }
+
+    private void setPersonalPanel() {
+
+    }
+
+    private void setMealPanel() {
+
+    }
+
+    private void setStudentsPanel() {
+
+    }
+
+    private void setProfessorsPanel() {
+
+    }
+
+    private void setClassRoomsPanel() {
+
+    }
+
+    private String[][] getStudents() {
+
+    }
+
+    private String[][] getProfessors() {
+
+    }
+
+    private void addStudent() {
+
+    }
+
+    private void addProfessor() {
+
+
+    }
+
+    private String[][] getClassrooms() {
+
+    }
+
+
 }
+
+
