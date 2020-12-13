286707dc5de873802ceaf4e7392059a23cd5eadb
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 13:04:09 +0330

    add essential methods to the student class

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student extends Person {
+
+    private int balance;
+    private List<Unit> units;
+    private Meal[] meals;
+
+    public Student() {
+        meals = new Meal[7];
+        units = new ArrayList<>();
+    }
+
+    public void deposit(int amount) {
+
+    }
+
+    public Meal[] getmeals() {
+
+    }
+
+    public boolean reserveMeal(Meal meal) {
+
+    }
+
+    public boolean addUnit(Classroom classroom) {
+
+
+
+    }
+
+    public void setBalance(int balance) {
+
+    }
+
+    public int getBalance() {
+
+    }
+
+    public List<Unit> getUnits() {
+
+    }
+
+    public double averageGrade() {
+
+
+}
+
+
 }
