400e6773f85f7399295c5030b0532acbef839ee6
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 16:25:33 +0330

    constructor of LoginPage completed

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -11,7 +11,24 @@ public class LoginPage {
     private JButton btnSubmit;
 
     public LoginPage() {
+        frame = new JFrame("Login");
+        frame.setBounds(100, 100, 410, 410);
+        frame.setLayout(null);
+        JPanel background = new JPanel(null);
+        background.setBounds(0, 0, 500, 500);
+        background.setBackground(Color.GRAY);
 
+        panel = new JPanel(null);
+        panel.setBounds(50, 90, 300, 200);
+
+        setComponents();
+
+
+        background.add(panel);
+        frame.add(background);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     private void setComponents() {
@@ -19,6 +36,5 @@ public class LoginPage {
     }
 
     private int login(String id, String password, String role) {
-
     }
 }
