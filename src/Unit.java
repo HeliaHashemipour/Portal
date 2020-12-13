80d532c4f0a922f7742ad5c1291caf5d0a756636
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 13:15:09 +0330

    complete Unit class

--- src/Unit.java
+++ src/Unit.java
@@ -1,2 +1,20 @@
 public class Unit {
+    private int grade;
+    private Classroom classroom;
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public Classroom getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(Classroom classroom) {
+        this.classroom = classroom;
+    }
 }
