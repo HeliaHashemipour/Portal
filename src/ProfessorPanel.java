319c5bad1b177c38b56d437d5cbc7ebaff66392b
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 13:30:20 +0330

    add essential methods to the ProfessorPanel class

--- src/ProfessorPanel.java
+++ src/ProfessorPanel.java
@@ -1,2 +1,63 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.util.List;
+
 public class ProfessorPanel {
+
+    private Professor professor;
+    private JFrame frame;
+    private JPanel header;
+    private JPanel menu;
+    private JPanel mainPanel;
+    private JPanel personalPanel;
+    private JPanel classRoomsPanel;
+    private JPanel studentsPanel;
+
+    public ProfessorPanel(Professor professor) {
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private void setHeader() {
+
+    }
+
+    private void setMenu() {
+
+    }
+
+    private void setMainPanel() {
+
+    }
+
+    private void setStudentsPanel() {
+
+    }
+
+    private void setClassRoomsPanel() {
+
+    }
+
+    private void addClassRoom() {
+
+    }
+
+    private Object[][] getClassrooms() {
+
+    }
+
+    private Object[][] getStudents(Classroom classroom) {
+
+    }
+
+    private void setStudentsGrade(Student st, Classroom classroom) {
+     
+    }
+
 }
+
+
