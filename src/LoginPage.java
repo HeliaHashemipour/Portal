04e0e9703ca441c4466a58a34c1a331dffb37651
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 16:26:24 +0330

    setComponents method of LoginPPage class completed

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -32,7 +32,52 @@ public class LoginPage {
     }
 
     private void setComponents() {
+        JLabel lblId, lblPassword, lblRole;
+        lblId = new JLabel("Username:");
+        lblId.setBounds(10, 40, 100, 20);
+        panel.add(lblId);
 
+        txtId = new JTextField();
+        txtId.setBounds(100, 40, 170, 23);
+        panel.add(txtId);
+
+        lblPassword = new JLabel("Password:");
+        lblPassword.setBounds(10, 80, 100, 20);
+        panel.add(lblPassword);
+
+        txtPassword = new JPasswordField();
+        txtPassword.setBounds(100, 80, 170, 23);
+        panel.add(txtPassword);
+
+        lblRole = new JLabel("Role:");
+        lblRole.setBounds(10, 120, 100, 20);
+        panel.add(lblRole);
+
+        cmbRole = new JComboBox<>();
+        cmbRole.addItem("Admin");
+        cmbRole.addItem("Professor");
+        cmbRole.addItem("Student");
+        cmbRole.setBounds(100, 120, 170, 23);
+        panel.add(cmbRole);
+
+        btnSubmit = new JButton("Login");
+        btnSubmit.setBounds(190, 160, 90, 25);
+        btnSubmit.addActionListener(e -> {
+            int login = login(txtId.getText(), txtPassword.getText(), cmbRole.getSelectedItem().toString());
+            Border border = BorderFactory.createLineBorder(Color.RED);
+            switch (login) {
+                case -1:
+                    txtId.setBorder(border);
+                    break;
+                case 0:
+                    txtPassword.setBorder(border);
+                    break;
+                case 1:
+                    frame.dispose();
+                    break;
+            }
+        });
+        panel.add(btnSubmit);
     }
 
     private int login(String id, String password, String role) {
