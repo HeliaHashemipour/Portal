cddd1a358c0ea553fc00e70e1e2ef5f822306ddb
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-13 13:10:53 +0330

    complete MealPlan class

---
+++ src/MealPlan.java
@@ -0,0 +1,19 @@
+public class MealPlan {
+    private static Meal[] meals;
+
+    public static void setMeals(Meal[] meals) {
+        MealPlan.meals = meals;
+    }
+
+    public static Meal[] getMeals() {
+        return meals;
+    }
+
+    public static int indexOf(Meal meal) {
+        for (int i = 0; i < 7; i++) {
+            if (meals[i].equals(meal))
+                return i;
+        }
+        return -1;
+    }
+}
