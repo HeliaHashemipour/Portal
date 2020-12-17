05c6a41279cc1e6a562d10b4a4f627ec030d2892
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-17 12:05:43 +0330

    complete Student class

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +14,7 @@ public class Student extends Person {
     }
 
     public void deposit(int amount) {
-
+        balance += amount;
     }
 
     public Meal[] getmeals() {
@@ -21,8 +22,9 @@ public class Student extends Person {
     }
 
     public boolean reserveMeal(Meal meal) {
-        if (meal.addStudent(this) && MealPlan.indexOf(meal) != -1) {
+        if (meal.getCost() < balance && meal.addStudent(this) && MealPlan.indexOf(meal) != -1) {
             this.meals[MealPlan.indexOf(meal)] = meal;
+            balance -= meal.getCost();
             return true;
         }
         else
@@ -31,20 +33,19 @@ public class Student extends Person {
 
     public boolean addUnit(Classroom classroom) {
         int numberOfUnits = 0;
-        int averageGrade = 0, totalGrade = 0;
-        for (int i = 0; i < this.getUnits().size(); i++){
-            int currentUnitNumber = this.getUnits().get(i).getClassroom().getNumberOfUnit();
-            numberOfUnits += currentUnitNumber;
-            totalGrade += this.getUnits().get(i).getGrade()*currentUnitNumber;
+        for (Unit unit : units) {
+            numberOfUnits += unit.getClassroom().getNumberOfUnit();
         }
-        averageGrade = totalGrade/numberOfUnits;
-        if (averageGrade < 17){
-            if (numberOfUnits + classroom.getNumberOfUnit() <= 20)
-                return this.units.add(classroom.getUnit());
+        double averageGrade = averageGrade();
+        if (averageGrade < 17 && numberOfUnits + classroom.getNumberOfUnit() <= 20) {
+            Unit unit = new Unit();
+            unit.setClassroom(classroom);
+            return this.units.add(unit);
         }
-        if (averageGrade >= 17){
-            if (numberOfUnits + classroom.getNumberOfUnit() <= 24)
-                return this.units.add(classroom.getUnit());
+        if (averageGrade >= 17 && numberOfUnits + classroom.getNumberOfUnit() <= 24) {
+            Unit unit = new Unit();
+            unit.setClassroom(classroom);
+            return this.units.add(unit);
         }
         return false;
     }
@@ -65,9 +66,10 @@ public class Student extends Person {
         double sum = 0;
         double unitSum = 0;
         for (Unit unit : units) {
-            sum += unit.getGrade();
+            sum += unit.getGrade() * unit.getClassroom().getNumberOfUnit();
             unitSum += unit.getClassroom().getNumberOfUnit();
         }
-        return sum / unitSum;
+        return unitSum == 0 ? 0 : sum / unitSum;
     }
 }
+
