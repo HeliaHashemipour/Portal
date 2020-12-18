461ce03c43b3d627b7fa6d7f23699e09a745cf37
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-12-18 17:23:13 +0330

    admin class completed

--- src/Admin.java
+++ src/Admin.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
 import java.util.List;
 
 public class Admin extends Person {
+
     public void setMealPlan(Meal[] meals) {
         MealPlan.setMeals(meals);
     }
@@ -14,9 +16,14 @@ public class Admin extends Person {
     }
 
     public boolean addProfessor(Professor professor) {
+        if (FileInterface.exists(professor.getId(), new Professor()))
+            return false;
         return FileInterface.addProfessor(professor);
     }
 
-    public boolean addStudent(Student student) { return FileInterface.addStudent(student);
+    public boolean addStudent(Student student) {
+        if (FileInterface.exists(student.getId(), new Student()))
+            return false;
+        return FileInterface.addStudent(student);
     }
 }
